d6dc9148b43e4d7d385686b83364f19a8ad1d23a
Daniel280200 <danieler@post.jce.ac.il>
2023-01-01 22:22:57 +0200

    fb-env

--- docs/js/fb-auth-init.js
+++ docs/js/fb-auth-init.js
@@ -1,5 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-import firebaseConfig from "./fb-env.js";
-const app=initializeApp(firebaseConfig);
+// import firebaseConfig from "./fb-env.js";
+const app=initializeApp({
+    apiKey: "AIzaSyAy0z3s7EfbcE7kWfcHWbYKGEt2CH4DY3Y",
+    authDomain: "orderbyit.firebaseapp.com",
+    projectId: "orderbyit",
+    storageBucket: "orderbyit.appspot.com",
+    messagingSenderId: "818501790937",
+    appId: "1:818501790937:web:44199c85834466471ba2c8",
+    measurementId: "G-MT9GP9DDLY"
+});
 export {app};
 export default app;
